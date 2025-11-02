f22a176c988c94249fda5c37030ad2418bf9ca5a
Pradeep M <pradeepmuthu1526@gmail.com>
2025-11-02 13:51:12 +0530

    pushed

---
+++ TAILWINDCSS CUSTOMIZATION/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "pradeep": "skyblue",
+        "venkat":"\#800020",
+     
+      },
+      fontSize:{
+        "siva":"50px"
+      }
+    },
+  },
+  plugins: [],
+}
+
